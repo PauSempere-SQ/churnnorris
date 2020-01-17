976cd76a4057ed23a24cfd9118379ec06518e483
Pau Sempere <psempere@solidq.com>
2020-01-17 19:20:26 +0100

    blog notebook 1 added

--- 01_baseline.py
+++ 01_baseline.py
@@ -9,6 +9,8 @@ from sklearn import ensemble
 from sklearn import preprocessing as sk_prep
 from sklearn.metrics import roc_auc_score, roc_curve, confusion_matrix
 import matplotlib.pyplot as plt
+from IPython.display import HTML
+import seaborn as sns
 
 #%%
 #load data from our local data folder
@@ -19,7 +21,7 @@ file = "churn_demo.csv"
 #personal information without any relation with their churn likelihood
 df_base = pd.read_csv(base_path+file).drop(columns=['RowNumber', 'Surname', 'CustomerId'], axis = 1)
 
-df_base.head(10)
+HTML(df_base.head(20).to_html(index = False))
 
 #%%
 #how much data do we have?
@@ -33,7 +35,31 @@ df_columns.remove(label)
 print(df_columns)
 
 #%%
-#encoders 
+#some EDA 
+
+#detect numeric and categorical data
+num_cols = list(df_base._get_numeric_data().columns)
+num_cols.remove(label)
+#use our numerical columns to find out our categorical columns
+cat_cols = list(set(df_base.columns) - set(num_cols))
+
+print(num_cols)
+
+#for numeric cols, plot bloxplots hued by the target
+for c in num_cols:
+    plt.figure(figsize=(8,8)) #create new figure
+    ax = sns.boxplot(x=label, y=c,  data=df_base) #plot histogram
+
+#%%
+#for categorical data create histograms
+print(cat_cols)
+
+for c in cat_cols:
+    plt.figure(figsize=(8, 8))
+    ax = sns.countplot(x=c, hue=label, data=df_base) 
+
+#%%
+#encoders. Just for the sake of the demo, other encoding strategies would be beneficial too
 l_binarizer = sk_prep.LabelBinarizer()
 str_encoder = sk_prep.OrdinalEncoder()
 
@@ -51,13 +77,11 @@ X_encoded.head(10)
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X_encoded, y_encoded, test_size = 0.3)
 
 print(X_train.shape, X_test.shape)
-
 #%%
 #fit a random forest with 100 trees of maximum 8 levels
 rf = ensemble.RandomForestClassifier(n_estimators = 100, 
         max_depth=8, criterion="entropy", n_jobs=-1)
 rf.fit(X = X_train, y = y_train.ravel())
-
 #%%
 #function to plot the ROC curve
 def get_roc_curve(X_train, y_train, X_test, y_test, model): 
@@ -88,7 +112,6 @@ print("Precision: ", round(metrics.precision_score(y_true = preds_rf, y_pred = y
 print("Recall: ", round(metrics.recall_score(y_true = preds_rf, y_pred = y_test.ravel()), 2))
 print("F1: ", round(metrics.f1_score(y_true = preds_rf, y_pred = y_test.ravel()), 2))
 print("Confusion matrix: \n", confusion_matrix(y_test, preds_rf))
-
 #%%
 #get the roc curve to check if we are better than a random classifier
 get_roc_curve(X_train, y_train,X_test, y_test, rf)
